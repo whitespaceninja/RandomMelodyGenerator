d79f1278eb0099526c35fb7a71f27fed7e9f58c2
Mike <mfriedmanisgod@netscape.net>
2015-04-28 15:16:14 -0400

    Nothing working yet. Still just researching and trying to take an approach to get one note to play. the code i have seems to play 2 sounds at once on 2 different threads, but i have some "using" issues that i have to correct still. needs ALOT of work.

--- RandomSongGenerator/Form1.Designer.cs
+++ RandomSongGenerator/Form1.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PlayButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayButton.Location = new System.Drawing.Point(68, 40);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(180, 77);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Play Sound Foo!!!";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(820, 386);
+            this.Controls.Add(this.PlayButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +56,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button PlayButton;
     }
 }
 
--- RandomSongGenerator/Form1.cs
+++ RandomSongGenerator/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+
+
+
 
 namespace RandomSongGenerator
 {
@@ -22,6 +26,37 @@ namespace RandomSongGenerator
 
         }
 
-        public static void Beep();
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            var p1 = new System.Media.Windows.MediaPlayer();
+            p1.Open(new System.Uri(@"C:\windows\media\tada.wav"));
+            p1.Play();
+
+            // this sleep is here just so you can distinguish the two sounds playing simultaneously
+            System.Threading.Thread.Sleep(500);
+
+            var p2 = new System.Windows.Media.MediaPlayer();
+            p2.Open(new System.Uri(@"C:\windows\media\tada.wav"));
+            p2.Play();
+
+            new System.Threading.Thread(() =>
+            {
+                var c = new System.Windows.Media.MediaPlayer();
+                c.Open(new System.Uri(@"C:\windows\media\tada.wav"));
+                c.Play();
+            }).Start();
+
+            System.Threading.Thread.Sleep(500);
+
+            new System.Threading.Thread(() =>
+            {
+                var c = new System.Windows.Media.MediaPlayer();
+                c.Open(new System.Uri(@"C:\windows\media\tada.wav"));
+                c.Play();
+            }).Start();
+        }
+
+        
+        
     }
 }
